f00123409d1490e7431213116081a1f71e6d66c8
Peter Gaultney <pgaultney@xoi.io>
2020-05-18 11:55:56 -0500

    cleaning up ssm parameters API

--- xoto3/ssm/parameters.py
+++ xoto3/ssm/parameters.py
@@ -2,21 +2,30 @@
 operations, which also transparently supports splitting large payloads
 across multiple parameters and joining them upon get using this same
 utility.
+
+Capital-letter arguments are SSM Parameter store arguments, whereas
+lowercase arguments are for our internal logic.
 """
+from typing import Union
 import re
 import os
 import os.path
 import json
 from datetime import datetime
+from typing_extensions import Literal
 
 from botocore.exceptions import ClientError
 
 from xoto3.lazy import tlls
 
+ParameterType = Union[Literal["String"], Literal["SecureString"], Literal["StringList"]]
+
+SSM_NOT_ALLOWED_REGEX = re.compile("[^0-9a-zA-Z_.-]+")
+# not actually used, but nice to have this defined here
 
 _MULTIPART_PARAM_COUNT = "__SSM_MULTIPART_COUNT"
+_MAX_PARAM_SIZE = 4096
 
-_SSM_NOT_ALLOWED_REGEX = re.compile("[^0-9a-zA-Z_.-]+")
 
 try:
     import __main__ as main_func
@@ -28,13 +37,13 @@ except Exception:  # pylint: disable=broad-except  # this is okay because gettin
 SSM_CLIENT = tlls("client", "ssm")
 
 
-def delete(name: str, ssm=None) -> bool:
+def delete(Name: str, ssm=None) -> bool:
     """Returns True if parameter was deleted,
     False if it didn't exist,
     and raises for other errors"""
     ssm = ssm or SSM_CLIENT()
     try:
-        ssm.delete_parameter(Name=name)
+        ssm.delete_parameter(Name=Name)
     except ClientError as ce:
         if _get_client_error_type(ce) == "ParameterNotFound":
             return True
@@ -42,7 +51,7 @@ def delete(name: str, ssm=None) -> bool:
     return True
 
 
-def get(name: str, default=None, error=True, ssm=None, **kwargs) -> str:
+def get(Name: str, default=None, error=True, ssm=None, **kwargs) -> str:
     """General utility for getting SSM parameter values.
 
     Returns only the actual value, not the full boto3 response.
@@ -56,7 +65,7 @@ def get(name: str, default=None, error=True, ssm=None, **kwargs) -> str:
     ssm = ssm or SSM_CLIENT()
 
     try:
-        param = ssm.get_parameter(Name=name, **kwargs)
+        param = ssm.get_parameter(Name=Name, **kwargs)
     except ClientError as ce:
         if _get_client_error_type(ce) != "ParameterNotFound":
             raise ce  # always raise if we've having unusual difficulties
@@ -69,13 +78,13 @@ def get(name: str, default=None, error=True, ssm=None, **kwargs) -> str:
     param_part_count = _get_num_multiparts(param_value)
     if param_part_count:
         try:
-            return _get_multipart_param(name, param_part_count, ssm)
+            return _get_multipart_param(Name, param_part_count, ssm)
         except (KeyError, ValueError, ClientError) as e:
-            raise RuntimeError(f"SSM multipart param {name} is not complete", e)
+            raise RuntimeError(f"SSM multipart param {Name} is not complete", e)
     return param_value
 
 
-def put(Name: str, Value: str, Type="String", Overwrite=True, ssm=None, **kwargs):
+def put(Name: str, Value: str, Type: ParameterType = "String", Overwrite=True, ssm=None, **kwargs):
     """General utility for putting SSM parameter values.
 
     Handles large/multipart puts transparently.
@@ -84,17 +93,12 @@ def put(Name: str, Value: str, Type="String", Overwrite=True, ssm=None, **kwargs
     """
     ssm = ssm or SSM_CLIENT()
 
-    if len(Value) > 4096:
-        _put_multipart_param(Name, Value, ssm, Type)
+    if len(Value) > _MAX_PARAM_SIZE:
+        _put_multipart_param(ssm, Name, Value, Type)
     else:
         if "Description" not in kwargs:
             kwargs["Description"] = f"Set by {SCRIPT_NAME} at " + datetime.now().isoformat()
         ssm.put_parameter(Name=Name, Value=Value, Type=Type, Overwrite=Overwrite, **kwargs)
-
-
-def ssm_allowed_name(unsafe_name: str) -> str:
-    """Replace characters that aren't allowed as part of an SSM name"""
-    return _SSM_NOT_ALLOWED_REGEX.sub("_", unsafe_name)
 
 
 def _get_client_error_type(clienterror: ClientError):
@@ -127,7 +131,7 @@ def _get_multipart_param_part_name(name, part_num):
     return f"{name}__{part_num}"
 
 
-def div_ceil(a, b):
+def _div_ceil(a, b):
     val = -(-a // b)
     assert val * b >= a
     return val
@@ -145,8 +149,8 @@ def _delete_multipart_param(name, ssm, count=None):
                 raise ce  # indicates a more serious problem than just a missing piece
 
 
-def _put_multipart_param(name, value, ssm, Type):
-    num_parts = div_ceil(len(value), 4096)
+def _put_multipart_param(ssm, name: str, value: str, Type: ParameterType):
+    num_parts = _div_ceil(len(value), _MAX_PARAM_SIZE)
     thistime = datetime.now().isoformat()
 
     put_param_payload = dict(
@@ -172,7 +176,7 @@ def _put_multipart_param(name, value, ssm, Type):
         des = f"Part {i+1} of {num_parts} of {name}, stored at " + thistime + f", by {SCRIPT_NAME}"
         ssm.put_parameter(
             Name=_get_multipart_param_part_name(name, i),
-            Value=value[i * 4096 : (i + 1) * 4096],
+            Value=value[i * _MAX_PARAM_SIZE : (i + 1) * _MAX_PARAM_SIZE],
             Description=des,
             Overwrite=True,
             Type=Type,
