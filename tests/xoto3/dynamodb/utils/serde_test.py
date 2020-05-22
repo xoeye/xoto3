98190b50ee799c5bdfd0014a4e772528cbe4d4f1
Peter Gaultney <pgaultney@xoi.io>
2020-05-21 21:55:55 -0500

    fixed test after refactor

--- tests/xoto3/dynamodb/utils/serde_test.py
+++ tests/xoto3/dynamodb/utils/serde_test.py
@@ -1,6 +1,6 @@
-from xoto3.dynamodb.utils.serde import dynamodb_prewrite_str_in_dict_transform
+from xoto3.dynamodb.utils.serde import dynamodb_prewrite_empty_str_in_dict_to_null_transform
 
 
 def test_no_empty_strings_in_maps():
     d = dict(a="", b="b")
-    assert dynamodb_prewrite_str_in_dict_transform(d) == dict(a=None, b="b")
+    assert dynamodb_prewrite_empty_str_in_dict_to_null_transform(d) == dict(a=None, b="b")
