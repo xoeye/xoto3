7fe7644bd756358b8f79eab5f436a88c079d9764
Peter Gaultney <pgaultney@xoi.io>
2020-05-23 12:46:52 -0500

    setup descrip

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ PKG_NAME = "xoto3"
 about: dict = dict()
 exec(open(f"{PKG_NAME}/__about__.py").read(), about)
 
+with open("README.md") as fh:
+    long_description = fh.read()
+
 setup(
     name=PKG_NAME,
     version=about["__version__"],
     author=about["__author__"],
     author_email=about["__author_email__"],
+    url="https://github.com/xoeye/xoto3",
     description="High level utilities for a subset of boto3 operations common for AWS serverless development in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     package_data={"": ["py.typed"]},
     python_requires=">=3.6",
