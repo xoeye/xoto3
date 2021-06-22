366b3f719a1127b5db522dda70031953b95bbaee
Peter Gaultney <pgaultney@xoi.io>
2021-06-22 12:08:50 -0500

    cloudwatch log scraper supports end time, and watch utility support custom LogReducer implementations

--- dev-utils/watch-cloudwatch-log-stream.py
+++ dev-utils/watch-cloudwatch-log-stream.py
@@ -1,27 +1,75 @@
 #!/usr/bin/env python
+"""
+
+"""
 import argparse
+import sys
 from datetime import datetime, timezone
 
 import boto3
 
 from xoto3.cloudwatch.logs import yield_filtered_log_events
+from xoto3.utils.dt import iso8601strict, parse8601strict
+
+DEFAULT_LOG_REDUCER = """
+class LogReducer:
+    def __init__(self):
+        self.events = 0
+
+    def __call__(self, log_event):
+        self.events += 1
+        msg = log_event["message"]
+        if not msg.startswith("REPORT RequestId"):
+            msg = msg.rstrip("\\n")
+        print(msg)
+
+    def __exit__(self):
+        print(f"{self.events} events")
+"""
+
+
+def exec_log_reducer(s: str):
+    try:
+        code = open(s).read()
+    except:  # noqa
+        code = s
+    exec(code, globals())
+    return LogReducer()  # noqa
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("log_group_name")
     parser.add_argument("--filter-pattern", "-f", default="")
+    parser.add_argument("--start-time", "-s", default=iso8601strict(datetime.now(timezone.utc)))
+    parser.add_argument("--end-time", "-e", default="")
+    parser.add_argument("--log-reducer", default=DEFAULT_LOG_REDUCER)
     args = parser.parse_args()
 
     cw_client = boto3.client("logs")
+    start_time = parse8601strict(args.start_time, aware=True)
 
-    start_time = datetime.now(timezone.utc)
+    events = 0
 
-    for log_event in yield_filtered_log_events(
-        cw_client, args.log_group_name, start_time, args.filter_pattern
-    ):
-        print(log_event["message"])
+    log_reducer = exec_log_reducer(args.log_reducer)
+
+    try:
+        for log_event in yield_filtered_log_events(
+            cw_client,
+            args.log_group_name,
+            start_time,
+            args.filter_pattern,
+            end_time=parse8601strict(args.end_time) if args.end_time else None,
+        ):
+            events += 1
+            log_reducer(log_event)
+    except KeyboardInterrupt:
+        pass
+    exit = getattr(log_reducer, "__exit__", None)
+    if exit:
+        exit()
+    return events
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(0 if main() else -1)
--- xoto3/cloudwatch/logs/events.py
+++ xoto3/cloudwatch/logs/events.py
@@ -1,10 +1,13 @@
 import time
 import typing as ty
 from datetime import datetime
+from logging import getLogger
 
 from typing_extensions import TypedDict
 
 from xoto3.paginate import PagePathsTemplate, yield_pages_from_operation
+
+_log = getLogger(__name__)
 
 
 class LogEvent(TypedDict):
@@ -28,14 +31,23 @@ def yield_filtered_log_events(
     log_group_name: str,
     start_time: datetime,
     filter_pattern: str = "",
+    *,
+    end_time: ty.Optional[datetime] = None,
     watch_interval: float = 1.0,
     # set to a negative number or 0 to stop yielding events when
     # the end of the stream is reached.
 ) -> ty.Iterator[FilteredLogEvent]:
-    """This will iterate infinitely unless watch_interval is set to 0 or negative."""
+    """This will iterate infinitely unless watch_interval is set to 0 or
+    negative, or end_time is provided.
+    """
     req = dict(logGroupName=log_group_name, startTime=int(start_time.timestamp() * 1000))
     if filter_pattern:
         req["filterPattern"] = filter_pattern
+    if end_time:
+        end_utc = end_time.timestamp()
+        req["endTime"] = int(end_utc * 1000)
+    else:
+        end_utc = 0
 
     def intercept_nextToken(next_token):
         """When filter_log_events returns an empty nextToken, that means it
@@ -43,9 +55,13 @@ def yield_filtered_log_events(
         you can hang on to the previous nextToken and resume calling
         later on, and you should pick up where you left off with new
         log events.
+
+        We don't want to do that, however, if we're already past the
+        end time specified by the caller and we're receiving empty
+        nextTokens, meaning that we've completed our scan.
         """
 
-        if next_token:
+        if next_token or (end_utc and end_utc < datetime.utcnow().timestamp()):
             req["nextToken"] = next_token
 
     while True:
@@ -56,7 +72,10 @@ def yield_filtered_log_events(
             last_evaluated_callback=intercept_nextToken,
         )
         for page in pages:
+            _log.debug(str(req))
             yield from page["events"]
         if watch_interval <= 0:
+            break
+        if req.get("nextToken") is None:
             break
         time.sleep(watch_interval)
